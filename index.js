73bed918a70383d9c9057ebd78a23374bf18677e
Montassir <montassirbendot@gmail.com>
2025-02-04 12:27:29 +0100

    gg

--- index.js
+++ index.js
@@ -9,9 +9,4 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-<<<<<<< HEAD
-//test restrictions
-=======
-//ss
-//ee
->>>>>>> 75c5b8e267681c9fc3decb3c8f2229ee9f157347
+
